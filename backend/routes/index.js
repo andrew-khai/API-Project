07212ebd27df2dc195f5026dae1074f3f420ca03
Andrew <1andrew.khai@gmail.com>
2023-07-06 16:24:35 -0700

    Add User login backend endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,14 @@
+// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
@@ -9,11 +18,5 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
-
-//add API router
-const apiRouter = require('./api');
-
-//API router
-router.use('/api', apiRouter);
 
 module.exports = router;
