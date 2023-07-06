d80f3137f2da7a2b5f2c4059afa51b263058a6b4
Andrew <1andrew.khai@gmail.com>
2023-07-06 15:49:25 -0700

    Add User authentication Express middleware

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -9,7 +9,6 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
 
-
   User.init(
     {
       username: {
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,18 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
+
+// GET /api/set-token-cookie
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
 
 module.exports = router;
