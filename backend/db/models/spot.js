84efebf6c3a6733a5211a8f2c02a219df931eb63
Andrew <1andrew.khai@gmail.com>
2023-07-10 12:29:39 -0700

    Added seed data for spots

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -90,7 +90,8 @@ module.exports = (sequelize, DataTypes) => {
       validate: {
         isInt: true
       }
-    }
+    },
+
   }, {
     sequelize,
     modelName: 'Spot',
---
+++ backend/db/seeders/20230710180438-demo-spots.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const { Spot } = require('../models');
+
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await Spot.bulkcreate([
+      {
+        ownerId: 1,
+        address: "123 Disney Lane",
+        city: "San Francisco",
+        state: "California",
+        country: "United States of America",
+        lat: 37.7645358,
+        lng: -122.4730327,
+        name: "App Academy",
+        description: "Place where web developers are created",
+        price: 123,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        ownerId: 2,
+        address: "0001 Milky Way Lane",
+        city: "Moon City",
+        state: "Space",
+        country: "Galaxy",
+        lat: 38.7645358,
+        lng: -121.4730327,
+        name: "Black Hole",
+        description: "Very quiet and spacious home. Gets really dark at night...and during the day.",
+        price: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        ownerId: 3,
+        address: "10 Seaside Lane",
+        city: "City of Atlantis",
+        state: "Atlantis",
+        country: "United States of America",
+        lat: -26.6841201,
+        lng: -122.0173401,
+        name: "Atlantis",
+        description: "Come swim with the fishes...if you dare.",
+        price: 75000,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ], {validate: true})
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
