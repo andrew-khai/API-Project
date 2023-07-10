2d3161a231f7b7293a23b8398456232f588a085f
Andrew <1andrew.khai@gmail.com>
2023-07-10 12:33:19 -0700

    Updated Spot seeder data

--- backend/db/seeders/20230710180438-demo-spots.js
+++ backend/db/seeders/20230710180438-demo-spots.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await Spot.bulkcreate([
+    await Spot.bulkCreate([
       {
         ownerId: 1,
         address: "123 Disney Lane",
