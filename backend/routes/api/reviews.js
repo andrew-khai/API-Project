ca56864b8eb2a2634de8293f804bd13d30196e2a
Andrew <1andrew.khai@gmail.com>
2023-07-13 13:09:01 -0700

    Updated Reviews for Current User and finished Edit review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -10,6 +10,16 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const validateReview = [
+  check('review')
+    .exists({ checkFalsy: true })
+    .withMessage(`Review text is required`),
+  check('stars')
+    .isInt({min: 1, max: 5})
+    .withMessage(`Stars must be an integer from 1 to 5`),
+  handleValidationErrors
+]
+
 //GET ALL Reviews of Current User
 router.get(
   '/current',
@@ -19,20 +29,85 @@ router.get(
     const userReviews = await Review.findAll({
       include: [
         {
-          model: Spot
+          model: User,
+          attributes: ['id', 'firstName', 'lastName']
         },
-        // {
-        //   model: ReviewImage
-        // }
+        {
+          model: Spot,
+          attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'],
+          include: SpotImage
+        },
+        {
+          model: ReviewImage,
+          attributes: ['id', 'url']
+        }
       ],
       where: {
         userId: user
       }
     })
 
-    res.json({
-      Reviews: userReviews
+    let allReviews = [];
+    // console.log(userReviews)
+    userReviews.forEach(review => {
+      allReviews.push(review.toJSON())
     })
+    // console.log(allReviews)
+    allReviews.forEach(review => {
+      // console.log(review)
+      // console.log(review.Spot.SpotImages)
+      review.Spot.SpotImages.forEach(spotImage => {
+        console.log(spotImage)
+        // console.log('preview', spotImage.preview)
+        if (spotImage.preview) {
+          review.Spot.previewImage = spotImage.url
+        }
+      })
+      delete review.Spot.SpotImages
+    })
+
+    res.json({
+      Reviews: allReviews
+    })
+  }
+)
+
+//Edit a Review
+router.put(
+  '/:reviewId',
+  validateReview,
+  async (req, res) => {
+    const userId = req.user.id;
+    if (!userId) {
+      res.status(401);
+      return res.json({
+        message: "Authentication required"
+      })
+    }
+
+    let userReview = await Review.findByPk(req.params.reviewId);
+    if (!userReview) {
+      res.status(404)
+      return res.json({
+        message: "Review couldn't be found"
+      })
+    }
+    if (userId !== userReview.userId) {
+      res.status(403);
+      return res.json({
+        message: "Forbidden"
+      })
+    }
+
+    const { review, stars } = req.body
+    if (userId === userReview.userId) {
+      if (review) userReview.review = review;
+      if (stars) userReview.stars = stars;
+    }
+
+    await userReview.save()
+
+    res.json(userReview)
   }
 )
 
