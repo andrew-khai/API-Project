4b4b66988c140291f5b648dc5fa27ec3eedfec2c
Andrew <1andrew.khai@gmail.com>
2023-07-13 10:11:41 -0700

    Finished Get All Reviews by Spot ID

--- backend/db/seeders/20230711035348-demo-review-images.js
+++ backend/db/seeders/20230711035348-demo-review-images.js
@@ -17,12 +17,16 @@ module.exports = {
       },
       {
         reviewId: 2,
-        url: "image url"
+        url: "images url"
       },
       {
         reviewId: 3,
         url: "image url"
       },
+      {
+        reviewId: 4,
+        url: 'image url'
+      }
     ])
   },
 
