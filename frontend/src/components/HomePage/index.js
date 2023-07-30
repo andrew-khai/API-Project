f5902be7d2a3ddb732615bdfbfdc8dbc06a42e5a
Andrew <1andrew.khai@gmail.com>
2023-07-30 12:44:45 -0700

    Updated seed data/added star favicon for ratings

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -21,11 +21,11 @@ const HomePage = () => {
     <>
       <div id="spots-container">
         {spots.map(spot => (
-          <div className="spots-boxes">
-            <div className="spots-boxes-image">{spot.previewImage}</div>
+          <div className="spots-boxes" key={spot.id}>
+            <img className="spots-boxes-image" src={spot.previewImage} alt={spot.name}></img>
             <div className="spots-boxes-details">
               <div className="spots-boxes-name">{spot.name}</div>
-              <div className="spots-boxes-ratings"><i class="fa-solid fa-star fa-xs"></i> {spot.avgRating}</div>
+              <div className="spots-boxes-ratings"><i className="fa-solid fa-star fa-xs"></i> {spot.avgRating}</div>
               <div className="spots-boxes-city-state">{spot.city}, {spot.state}</div>
               <div className="spots-boxes-price">${spot.price}/day</div>
             </div>
