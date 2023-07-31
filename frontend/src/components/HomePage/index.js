0b33e7f3dd04618cefe79f62ba5d24aab9d53c6b
Andrew <1andrew.khai@gmail.com>
2023-07-30 23:01:08 -0700

    Added create a spot link user is logged in/updated CSS

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -24,9 +24,11 @@ const HomePage = () => {
           <div className="spots-boxes" key={spot.id}>
             <img className="spots-boxes-image" src={spot.previewImage} alt={spot.name}></img>
             <div className="spots-boxes-details">
-              <div className="spots-location-ratings"><i className="fa-solid fa-star fa-xs"></i> {spot.avgRating}</div>
-              <div className="spots-boxes-city-state">{spot.city}, {spot.state}</div>
-              <div className="spots-boxes-price">${spot.price}<span>/day</span></div>
+              <div className="spots-location-ratings">
+                <div className="spots-boxes-city-state">{spot.city}, {spot.state}</div>
+                <div className="spots-boxes-ratings"><i className="fa-solid fa-star fa-xs"></i> {spot.avgRating}</div>
+              </div>
+              <div className="spots-boxes-price">${spot.price}<span className="day-text"> /day</span></div>
             </div>
           </div>
         ))}
